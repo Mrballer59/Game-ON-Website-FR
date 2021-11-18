845baabcccfffffda19cbefb95d0f3ebb82f4763
B <77552838+Mrballer59@users.noreply.github.com>
2021-11-18 13:38:37 +0100

    first commit

---
+++ modal.js
@@ -0,0 +1,29 @@
+function editNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
+  // DOM Elements
+  const modalbg = document.querySelector(".bground");
+  const modalBtn = document.querySelectorAll(".modal-btn");
+  const formData = document.querySelectorAll(".formData");
+  const closeModal = document.querySelector('.close');
+  
+  // launch modal event
+  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+  
+  // launch modal form
+  function launchModal() {
+    modalbg.style.display = "block";
+  }
+
+  // close modal form
+closeModal.addEventListener('click', crossClose);
+
+function crossClose() {
+  modalbg.style.display = "none";
+}
