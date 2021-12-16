2cb71794b87334beb79ed12b35e932fcd94dd3b2
B <77552838+Mrballer59@users.noreply.github.com>
2021-12-16 09:34:07 +0100

    Finish work

--- modal.js
+++ modal.js
@@ -1,134 +1,205 @@
-// DOM Elements
-  const modalbg = document.querySelector(".bground");
-  const modalBtn = document.querySelectorAll(".modal-btn");
-  const formData = document.querySelectorAll(".formData");
-  const closeModal = document.querySelector('.close');
-
 function editNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  
-  // launch modal event
-  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-  
-  // launch modal form
-  function launchModal() {
-    modalbg.style.display = "block";
-  }
+}
 
-  // close modal form
-closeModal.addEventListener('click', crossClose);
+document.querySelector(".icon").addEventListener("click", editNav);
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form
+closeModal.addEventListener("click", crossClose);
 
 function crossClose() {
   modalbg.style.display = "none";
 }
 
-//Entey Elements
+// Validation
 
-const inputs = document.querySelectorAll('input[type="text"],input[type="email"]input[type="date"],input[type="number"]');
+// Retrieve nodes for validation
 
-const dateInput = document.queryElementById("birthday");
-
-const cities = document.querySelectorAll('input["type=radio"]');
-
-const citycheck = document.getElementById("citycheck");
-
+// Firstname
+const firstName = document.querySelector("#first");
+const prenomDiv = document.querySelector("#div-prenom");
+// Lastname
+const lastName = document.querySelector("#last");
+const nomDiv = document.querySelector("#div-nom");
+//Email
+const email = document.querySelector("#email");
+const emailDiv = document.querySelector("#div-email");
+// Birthdate
+const birthDate = document.querySelector("#birthdate");
+const birthDateDiv = document.querySelector("#birthdate-div");
+// n° of tournaments
+const tournamentNr = document.querySelector("#quantity");
+const tournamentDiv = document.querySelector("#tournament-div");
+// cities
+const radios = document.getElementsByName("location");
+const checkboxDiv = document.querySelector("#checkbox-div");
+// terms of use
+const checkbox1Div = document.querySelector("#checkbox1-div");
 const checkbox1 = document.querySelector("#checkbox1");
 
-const conditions = document.getElementById("conditionscheck");
+// Manage form submission
 
-const checkbox2 = document.querySelector("#checkbox2");
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-const submit = document.querySelector(".content");
+  //add a counter to check each form input
+  let counter = 0;
 
-const thanksPage = document.getElementById("thankspage");
+  // Validation conditions
 
-const closePageCross = document.getElementById("thanksclosecross");
+  // input values does not accept blanks
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
 
-const closePageButton = document.getElementById("thanksclosebutton");
-let first,
-  last,
-  email,
-  quantity,
-  town,
-  conditionAccepted,
-  newsletter,
-  birthdate;
+  //define email format
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+  //define tournamentNr format
+  const tournamentNrFormat = /\d+/g;
 
-//Function
+  // check firstname
 
-const errorDisplay = (tag, message, vaild) => {
+  if (firstNameValue.length < 2) {
+    // Error message
+    prenomDiv.setAttribute(
+      "data-error",
+      "Veuillez entrer 2 caractères, pour le champ du prenom"
+    );
+    prenomDiv.setAttribute("data-error-visible", "true");
+  } else {
+    prenomDiv.setAttribute("data-error-visible", "false");
+    counter++;
+  }
 
-    const container = document.querySelector("#" + tag);
-    const span = document.querySelector("#" + tag + " +span"); 
-    if (!vaild) {
-      container.classList.add("error");
-      span.classList.add("error-message");
-      span.textContent = message; 
-    } else {
-      container.classList.remove("error"); 
-      span.textContent = message;
+  // check lastname
+
+  if (lastNameValue.length < 2) {
+    nomDiv.setAttribute(
+      "data-error",
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom"
+    );
+    nomDiv.setAttribute("data-error-visible", "true");
+  } else {
+    nomDiv.setAttribute("data-error-visible", "false");
+    counter++;
+  }
+
+  // check email
+
+  if (mailFormat.test(email.value)) {
+    emailDiv.setAttribute("data-error-visible", "false");
+    counter++;
+  } else {
+    emailDiv.setAttribute(
+      "data-error",
+      "Veuillez saisir une adresse mail correcte."
+    );
+    emailDiv.setAttribute("data-error-visible", "true");
+  }
+
+  // check date
+
+  if (birthDate.value === "") {
+    birthDateDiv.setAttribute("data-error", "Merci de donner une date valide");
+    birthDateDiv.setAttribute("data-error-visible", "true");
+  } else {
+    birthDateDiv.setAttribute("data-error-visible", "false");
+    counter++;
+  }
+
+  // check number of tournaments
+
+  if (tournamentNrFormat.test(tournamentNr.value)) {
+    tournamentDiv.setAttribute("data-error-visible", "false");
+    counter++;
+  } else {
+    tournamentDiv.setAttribute(
+      "data-error",
+      "Merci de donner un nombre entre 0 et 99"
+    );
+    tournamentDiv.setAttribute("data-error-visible", "true");
+  }
+
+  // check city
+
+  let radioValid = false;
+
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      radioValid = true;
+      checkboxDiv.setAttribute("data-error-visible", "false");
+      counter++;
+      break;
     }
-};
-
-const firstChecker = (value) => {
-  if (value.length > 0 && value.length < 2) {
-    errorDisplay ( "first", "Veuillez entrer 2 caractères ou plaus pour le prénom.");
-    first = null; 
-  } else if (
-    !value.match( /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u)
-  ) {
-    errorDisplay("first", "Le prénom doit contenir des lettres uniquement.");
-    first = null;
-  } else {
-    errorDisplay("first", "", true);
-    first = value;
   }
-};
 
-const lastChecker = (value) => {
-  if (value.length > 0 && value.length < 2) {
-    errorDisplay("last", "Veuillez entrer 2 caractères ou plus pour le nom.");
-    last = null; 
-  }  else if 
-  ( !value.match ( /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u)
-  ) {
-    errorDisplay("last", "Le nom doit contenir des lettres uniquement."); 
-    last = null; 
-  } else {
-    errorDisplay("last", "", true);
-    last = value;
+  if (!radioValid) {
+    checkboxDiv.setAttribute("data-error", "Merci de choisir une ville");
+    checkboxDiv.setAttribute("data-error-visible", "true");
   }
-};
 
-const emailChecker = (value) => {
-  if 
-  (!value.match (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
-  ){
-    errorDisplay("email", "Le Mail n'est pas valide"); 
-    email = null; 
+  // check usage conditions.
+
+  if (!checkbox1.checked) {
+    checkbox1Div.setAttribute(
+      "data-error",
+      "Merci de lire et d'accepter les conditions d'usage"
+    );
+    checkbox1Div.setAttribute("data-error-visible", "true");
   } else {
-    errorDisplay("email", "" , true); 
-    email = value;
-}
-}; 
+    checkbox1Div.setAttribute("data-error-visible", "false");
+    counter++;
+  }
 
+  // When the form is valid pass though and give success message
 
-const birthdateChecker = (value) => {
-  let date = new Date (value);
+  const bgrounDiv = document.querySelector(".bground");
+  const successDiv = document.querySelector("#success-div");
+  const closeSuccess = document.querySelector(".close-success-div");
+  const btnClose = document.querySelector(".btn-close");
 
-}
+  // when close success modal
+  closeSuccess.addEventListener("click", successCrossClose);
+  btnClose.addEventListener("click", closeBtn);
 
+  // when close success modal with cross
+  function successCrossClose() {
+    successDiv.style.display = "none";
+  }
 
+  // close success modal with button
+  function closeBtn() {
+    successDiv.style.display = "none";
+  }
 
+  // reset form
+  const validate = () => {
+    document.querySelector(".subscribe").reset();
+  };
 
-
-
-
-
+  // if all mandatory inputs are filled launch validate
+  if (counter === 7) {
+    bgrounDiv.style.display = "none";
+    successDiv.style.display = "block";
+    validate();
+  }
+});
